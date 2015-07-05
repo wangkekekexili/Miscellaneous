efb3333be85fd381a7caa780b4a64cbb2dc73a4b
Ke Wang <wangkekekexili@gmail.com>
2015-07-05 11:55:35 -0400

    Add GUI for the dictionary.

--- CambridgeDictionary/src/EnglishChineseDictionary.java
+++ CambridgeDictionary/src/EnglishChineseDictionary.java
@@ -1,5 +1,3 @@
-import java.util.StringJoiner;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -14,49 +12,32 @@ public class EnglishChineseDictionary {
 
 	private static final String URL = "http://dictionary.cambridge.org/us/dictionary/english-chinese-simplified/";
 	
-	public static final void showUsage() {
-		System.out.println("usage: dict [word]");
-		System.exit(1);
-	}
-	
-	public static final void showNoResult() {
-		System.out.println("No result.");
-		System.exit(1);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		if (args.length == 0) {
-			showUsage();
-		}
-		StringJoiner joiner = new StringJoiner("_");
-		for (String argument: args) {
-			joiner.add(argument);
-		}
-		final String wordToSearch = joiner.toString();
-		
+	public static String searchForDefination(String word) {
 		Document document = null;
 		try {
-			document = Jsoup.connect(URL+wordToSearch).get();
+			document = Jsoup.connect(URL+word).get();
 		} catch (Exception e) {
-			showNoResult();
+			return e.getMessage();
 		}
 		Elements titleElements = document
 				.getElementsByAttributeValue("class", "di-title cdo-section-title-hw");
 		if (titleElements.size() == 0) {
-			showNoResult();
+			return "No result found on Cambridge English Chinese dictionary.";
 		}
-		System.out.println(titleElements.get(0).text());
 		
+		StringBuilder resultBuilder = new StringBuilder();
 		Elements definationBodies = document
 				.getElementsByAttributeValue("class", "def-body");
 		for (int i = 0;i != definationBodies.size();i++) {
-			System.out.print(Integer.toString(i+1));
-			System.out.print(": ");
-			System.out.println(definationBodies.get(i)
+			resultBuilder.append(Integer.toString(i+1));
+			resultBuilder.append(": ");
+			resultBuilder.append(definationBodies.get(i)
 					.getElementsByTag("div").get(0)
 					.text().trim());
+			resultBuilder.append("\n");
 		}
 		
+		return resultBuilder.toString();
 	}
 
 }
---
+++ CambridgeDictionary/src/SimpleGui.java
@@ -0,0 +1,49 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class SimpleGui extends JFrame {
+	private JTextField wordToSearch;
+	private JTextArea resultArea;
+	public SimpleGui() {
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setSize(240, 320);
+		getContentPane().setLayout(null);
+		
+		wordToSearch = new JTextField();
+		wordToSearch.setBounds(52, 6, 134, 28);
+		getContentPane().add(wordToSearch);
+		wordToSearch.setColumns(10);
+		
+		JButton searchButton = new JButton("Search");
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String result = EnglishChineseDictionary.searchForDefination(
+						wordToSearch.getText().replaceAll(
+								Pattern.quote(" "), "_"));
+				resultArea.setText(result);
+			}
+		});
+		searchButton.setBounds(62, 40, 117, 29);
+		getContentPane().add(searchButton);
+		
+		resultArea = new JTextArea();
+		resultArea.setText("");
+		resultArea.setBounds(6, 81, 228, 211);
+		getContentPane().add(resultArea);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()->{
+			new SimpleGui().setVisible(true);;
+		});
+	}
+}
